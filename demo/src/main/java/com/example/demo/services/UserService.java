67426dee3fb7e2d27da184296f8c75a79f1ef9d3
Bushy.Netshidaulu <Bushy.Netshidaulu@03rnbc-ntpc0204.za.ds.naspers.com>
2018-04-06 11:52:31 +0200

    Spring simple rest api

---
+++ demo/src/main/java/com/example/demo/services/UserService.java
@@ -0,0 +1,23 @@
+package com.example.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepo;
+
+@Service
+public class UserService {
+
+	private UserRepo userRepository;
+	
+	@Autowired
+	public UserService(UserRepo userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	//to save a user 
+	public User addUser(User user) {		
+		return userRepository.save(user);
+	}
+}
