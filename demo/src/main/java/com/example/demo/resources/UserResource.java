3f58748d71d716d947196aef0f88bb8c661cce24
Bushy.Netshidaulu <Bushy.Netshidaulu@03rnbc-ntpc0204.za.ds.naspers.com>
2018-04-06 12:13:16 +0200

    get all users Resource

--- demo/src/main/java/com/example/demo/resources/UserResource.java
+++ demo/src/main/java/com/example/demo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.example.demo.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,11 @@ public class UserResource {
 	@PostMapping(path="user")
 	public User addUser(User user) {		
 		return userService.addUser(user);
+	}
+	
+	@GetMapping(path="users")
+	public Iterable<User> getAllUsers(){
+		return userService.findAllUsers();
 	}
 	
 
