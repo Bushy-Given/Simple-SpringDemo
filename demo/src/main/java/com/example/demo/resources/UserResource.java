8344bd7da0468d555d24ab6c7d476405561a15bc
Bushy.Netshidaulu <Bushy.Netshidaulu@03rnbc-ntpc0204.za.ds.naspers.com>
2018-04-06 12:06:53 +0200

    added method to add user resource

--- demo/src/main/java/com/example/demo/resources/UserResource.java
+++ demo/src/main/java/com/example/demo/resources/UserResource.java
@@ -1,12 +1,26 @@
 package com.example.demo.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
 
 @RestController
 @RequestMapping(path="api/")
 public class UserResource {
 	
+	private UserService userService; //dependecy injection
+	@Autowired
+	public UserResource(UserService userService) {
+		this.userService = userService;
+	}
+	@PostMapping(path="user")
+	public User addUser(User user) {		
+		return userService.addUser(user);
+	}
 	
 
 }
